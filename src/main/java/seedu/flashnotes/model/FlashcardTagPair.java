1c45191f2bdc76c378795f32a9534cbbd2d5afb7
Wong Peng Xiang <wong.pengxiang@gmail.com>
2020-10-06 15:21:29 +0800

    Add FlashcardTagPair class

--- src/main/java/seedu/flashnotes/model/FlashcardTagPair.java
+++ src/main/java/seedu/flashnotes/model/FlashcardTagPair.java
@@ -1,2 +1,23 @@
-package seedu.flashnotes.model;public class FlashcardTagPair {
+package seedu.flashnotes.model;
+
+import seedu.flashnotes.model.flashcard.Flashcard;
+import seedu.flashnotes.model.tag.Tag;
+
+public class FlashcardTagPair {
+
+    Flashcard flashcard;
+    Tag tag;
+
+    public FlashcardTagPair(Flashcard flashcard, Tag tag) {
+        this.flashcard = flashcard;
+        this.tag = tag;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public Flashcard getFlashcard() {
+        return flashcard;
+    }
 }
